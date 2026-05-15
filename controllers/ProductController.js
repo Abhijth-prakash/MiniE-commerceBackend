e0e98c691255f90fbbec5d1e43377fef8bbb1b8d
abhijith <abhijithabs125@gmail.com>
2026-05-15 13:37:37 +0530

    defined database schema

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,7 +1,12 @@
+const Products = require('../models/productModel')
 
-
-const HomePage = async (req,res)=>{
-    res.send("hi iam batman")
+const HomePage = async (req, res) => {
+    try {
+        const ProductData = await Products.find({})
+        res.json({ "products": ProductData })  
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 }
 
 
